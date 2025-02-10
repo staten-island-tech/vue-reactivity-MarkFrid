a99515fc74bbbb03388ceb594ec41ff5598a31fa
MarkFrid <mark.fridlin@gmail.com>
2025-02-10 12:31:36 -0500

    .,

--- app/src/components/TheNav.vue
+++ app/src/components/TheNav.vue
@@ -12,7 +12,7 @@
           >
           <RouterLink
             to="/TheBet"
-            class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+            class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
             >Did it hit?</RouterLink
           >
         </div>
